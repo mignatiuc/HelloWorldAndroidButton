9577109cd6749a905d98ab72e6166a1ce55fb70c
Maxim Ignatiuc <maxim.ignatiuc@utdallas.edu>
2015-02-06 09:54:21 -0600

    Update2

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.maxim_ignatiuc.helloworldbutton;
+package com.example.maxim_ignatiuc.helloworldandroidbutton;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -11,77 +12,84 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
+import com.example.maxim_ignatiuc.helloworldandroidbutton.R;
+
 
 public class MainActivity extends ActionBarActivity {
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
+        
         return super.onOptionsItemSelected(item);
     }
-
+    
     /////
-
+    
     @Override
     protected void onCreate(Bundle bundle) {
-
+        
         super.onCreate(bundle);
         setContentView(R.layout.activity_main);//attaches the layout to activity
-
+        
         final ViewGroup vg1 = (ViewGroup) findViewById(R.id.root1);
         float w1 = vg1.getWidth();
         float h1 = vg1.getHeight();
-
+        
         final Button b1 = new Button(this);
-
-//      b1.setWidth((int) 100);
-//        b1.setHeight((int) 30);
-
-        b1.setText("Hello");
-
-
+        b1.setBackgroundColor(Color.WHITE);
+        vg1.setBackgroundColor(Color.WHITE);
+        
+        //      b1.setWidth((int) 100);
+        //        b1.setHeight((int) 30);
+        
+        b1.setText("hello");
+        b1.setWidth(550);
+        b1.setHeight(180);
+        
+        
         //float left = (w1 - vg1.getWidth())/2;
         //float top = (h1 - vg1.getHeight())/2;
-
+        
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-       // layoutParams.addRule(100,30);
-
+        // layoutParams.addRule(100,30);
+        
         layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
         layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
-
-//        layoutParams.setMargins( (int)left,  (int)top, (int)(left+w1/2), (int)(top+h1/2));
-//        layoutParams.gravity = Gravity.CENTER;
-
+        
+        //        layoutParams.setMargins( (int)left,  (int)top, (int)(left+w1/2), (int)(top+h1/2));
+        //        layoutParams.gravity = Gravity.CENTER;
+        
         b1.setLayoutParams(layoutParams);
         vg1.addView(b1);
-
-
-
+        
+        
+        
+        
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (b1.getText().toString().equals("Hello")) {
-                    b1.setText("World");
+                if (b1.getText().toString().equals("hello")) {
+                    b1.setText("world");
                 } else {
-                    b1.setText("Hello");
+                    b1.setText("hello");
                 }
-
+                
             }
         });
     }
